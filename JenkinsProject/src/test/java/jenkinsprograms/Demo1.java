2f5f49f10490e083b5e3684f87783c2abbec704e
Netaji <Netaji@DESKTOP-LHVL74B>
2023-08-29 23:28:58 +0530

    1st push

---
+++ JenkinsProject/src/test/java/jenkinsprograms/Demo1.java
@@ -0,0 +1,11 @@
+package jenkinsprograms;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+@Test
+	public void demo1test()
+	{
+		System.out.println("hi");
+	}
+}
---
+++ JenkinsProject/src/test/java/jenkinsprograms/Demo2.java
@@ -0,0 +1,12 @@
+package jenkinsprograms;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	@Test
+	public void demo2Test()
+	{
+		System.out.println("hello");
+	}
+
+}
